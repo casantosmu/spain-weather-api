4bdaa55acba05443c7d10ab6b46f65183860a3ba
Carlos <casantosmu@gmail.com>
2023-03-08 18:44:59 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
