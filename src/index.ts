ba6d475c8a6dd31768e7dae25d9c0c7949d927b2
Carlos <casantosmu@gmail.com>
2023-03-08 18:58:43 +0100

    Added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
+import { type Request, type Response } from "express";
 import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT ?? 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
