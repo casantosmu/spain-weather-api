8c77b498d9afc534326aab78e085be30af6af858
Carlos <casantosmu@gmail.com>
2023-03-25 15:18:09 +0100

    Added openapi doc, ui and router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from "express";
 import express from "express";
 import dotenv from "dotenv";
+import openapiRouter from "./openapi/openapiRouter";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const port = process.env.PORT ?? 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+app.use(openapiRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
