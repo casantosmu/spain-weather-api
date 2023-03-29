4d68d1700c8624c60fa293cd8550b6086632a2ba
Carlos <casantosmu@gmail.com>
2023-03-29 15:09:07 +0200

    Deleted dotenv dependency and env file

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:18-slim
 WORKDIR /usr/src/app
 COPY package.json package-lock.json ./
 RUN npm install
-COPY tsconfig*.json .env ./
+COPY tsconfig*.json ./
 COPY src ./src
 
 RUN npm run build
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import { type Request, type Response } from "express";
 import express from "express";
-import dotenv from "dotenv";
-import docsRouter from "./docs/docsRouter";
 import router from "./router";
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 3000;
