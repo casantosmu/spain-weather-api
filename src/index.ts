270c3d7ad6152759bdeeab0351a8aa15d9aba9b9
Carlos Santos <casantosmu@gmail.com>
2023-04-13 23:27:30 +0200

    Added logger level

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import mongoose from "mongoose";
 import express from "express";
-import router from "./router";
 import pinoHttp from "pino-http";
-import pino from "pino";
+import pino, { type LoggerOptions } from "pino";
 
-const logger = pino();
-const httpLogger = pinoHttp();
+const loggerOptions: LoggerOptions = {
+  errorKey: "error",
+  messageKey: "message",
+  level: process.env["LOG_LEVEL"]!,
+};
+
+const logger = pino(loggerOptions);
+const httpLogger = pinoHttp(loggerOptions);
 
 const app = express();
 const port = process.env["SERVER_PORT"] ?? 3000;
 
 app.use(httpLogger);
-app.use("/v1", router);
+app.disable("x-powered-by");
 
 const handleError = (error: unknown) => {
   logger.error(error);
