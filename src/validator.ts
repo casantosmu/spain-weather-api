de85efd0bc34c3218e15b3926ce907a6110d991b
Carlos Santos <casantosmu@gmail.com>
2023-04-16 15:40:15 +0200

    Added basic validation: ajv instance and middleware

---
+++ src/validator.ts
@@ -0,0 +1,41 @@
+import { type Request, type Response, type NextFunction } from "express";
+import Ajv, { type ErrorObject, type ValidateFunction } from "ajv";
+
+// AJV Example
+//
+// const schema = {
+//   type: "object",
+//   properties: {
+//     foo: { type: "integer" },
+//     bar: { type: "string" },
+//   },
+//   required: ["foo"],
+//   additionalProperties: false,
+// };
+
+// export const validate = validator.compile(schema);
+
+export const validator = new Ajv({ allErrors: true });
+
+const convertAjvErrorToMessage = (ajvError: ErrorObject) => {
+  const propertyName = ajvError.instancePath.split("/").pop();
+  const errorMessage = ajvError.message ?? "Validation error";
+  return propertyName ? `"${propertyName}": ${errorMessage}` : errorMessage;
+};
+
+export const validateMiddleware =
+  (validateSchema: ValidateFunction) =>
+  (req: Request, _res: Response, next: NextFunction) => {
+    validateSchema(req.body);
+
+    const validationError = validateSchema?.errors?.[0];
+
+    if (validationError) {
+      const errorMessage = convertAjvErrorToMessage(validationError);
+      const error = new Error(errorMessage);
+      next(error);
+      return;
+    }
+
+    next();
+  };
---
+++ tests/validator.test.ts
@@ -0,0 +1,76 @@
+import { type Request, type Response } from "express";
+import { validateMiddleware } from "../src/validator";
+import { type ErrorObject, type ValidateFunction } from "ajv";
+
+describe("validateMiddleware", () => {
+  describe("when called", () => {
+    it("should call validateSchema with req.body", () => {
+      const req = { body: { foo: "bar" } };
+      const res = {};
+      const next = jest.fn();
+
+      const validateSchema = jest.fn() as unknown as ValidateFunction;
+
+      const middleware = validateMiddleware(validateSchema);
+      middleware(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(validateSchema).toHaveBeenCalledWith(req.body);
+    });
+  });
+
+  describe("when validateSchema has errors", () => {
+    it("should call next with an error", () => {
+      const req = { body: { foo: "bar" } };
+      const res = {};
+      const next = jest.fn();
+
+      const error = new Error("Validation error");
+
+      const validateSchema = jest.fn() as unknown as ValidateFunction;
+      validateSchema.errors = [{ instancePath: "" }] as ErrorObject[];
+
+      const middleware = validateMiddleware(validateSchema);
+      middleware(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("when validateSchema have error with instancePath and message", () => {
+    it("should call next with an error message including instancePath", () => {
+      const req = { body: { foo: "bar" } };
+      const res = {};
+      const next = jest.fn();
+
+      const instancePath = "/foo";
+      const errorMessage = "must be integer";
+      const error = new Error(`"foo": ${errorMessage}`);
+
+      const validateSchema = jest.fn() as unknown as ValidateFunction;
+      validateSchema.errors = [
+        { instancePath, message: errorMessage },
+      ] as ErrorObject[];
+
+      const middleware = validateMiddleware(validateSchema);
+      middleware(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("when validateSchema hasn't errors", () => {
+    it("should call next without arguments ", () => {
+      const req = { body: { foo: "bar" } };
+      const res = {};
+      const next = jest.fn();
+
+      const validateSchema = jest.fn() as unknown as ValidateFunction;
+
+      const middleware = validateMiddleware(validateSchema);
+      middleware(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith();
+    });
+  });
+});
