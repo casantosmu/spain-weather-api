e0054225f1f4d2111a0786b921220f9871865fde
Carlos Santos <casantosmu@gmail.com>
2023-06-04 13:20:42 +0200

    Remove mappers file

--- src/modules/location/locationRepository.ts
+++ src/modules/location/locationRepository.ts
@@ -1,10 +1,101 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   type LocationAutonomousCity,
   type LocationMunicipality,
   type LocationProvince,
 } from "./types";
 import { LocationModel, MunicipalityModel } from "./locationModels";
-import { mapToLocation, mapToLocationModel } from "./locationMappers";
+import { entity } from "./constants";
+
+export const mapToLocation = (
+  location: any
+): LocationMunicipality | LocationProvince | LocationAutonomousCity => {
+  const defaultData = {
+    id: location._id,
+    name: location.name,
+    latLng: [
+      location.geo2dPoint.coordinates[1],
+      location.geo2dPoint.coordinates[0],
+    ] as const,
+    code: location.code,
+    entity: location.entity,
+    year: location.year,
+  };
+
+  switch (location.entity) {
+    case entity.municipality:
+      return {
+        ...defaultData,
+        municipalityId: location.municipalityId,
+        province: {
+          id: location.province.provinceId,
+          name: location.province.name,
+          code: location.province.code,
+        },
+      };
+    case entity.province:
+      return {
+        ...defaultData,
+        provinceId: location.provinceId,
+        capital: {
+          id: location.capital.municipalityId,
+          name: location.capital.name,
+          code: location.capital.code,
+        },
+      };
+    default:
+      return {
+        ...defaultData,
+        autonomousCityId: location.autonomousCityId,
+      };
+  }
+};
+
+export const mapToLocationModel = (
+  location: LocationMunicipality | LocationProvince | LocationAutonomousCity
+) => {
+  const defaultData = {
+    _id: location.id,
+    name: location.name,
+    code: location.code,
+    geo2dPoint: {
+      type: "Point",
+      coordinates: [location.latLng[1], location.latLng[0]],
+    },
+    year: location.year,
+    entity: location.entity,
+  };
+
+  switch (location.entity) {
+    case entity.municipality:
+      return {
+        ...defaultData,
+        municipalityId: location.municipalityId,
+        province: {
+          provinceId: location.province.id,
+          name: location.province.name,
+          code: location.province.code,
+        },
+      };
+    case entity.province:
+      return {
+        ...defaultData,
+        provinceId: location.provinceId,
+        capital: {
+          municipalityId: location.capital.id,
+          name: location.capital.name,
+          code: location.capital.code,
+        },
+      };
+    case entity.autonomousCity:
+      return {
+        ...defaultData,
+        autonomousCityId: location.autonomousCityId,
+      };
+    default:
+      return defaultData;
+  }
+};
 
 export const createLocationsRepository = async (
   locations: Array<
